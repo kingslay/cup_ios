1dedeec9df4af2fc0e72d3be14377433f4da1e84
wangjinbian <wangjinbian@linggan.com>
2015-11-24 15:35:19 +0800

    no message

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,10 +1,29 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import XCPlayground
+
 
 var str = "Hello, playground"
-let lightGrey = UIColor(red: 0.9765, green: 0.9765, blue: 0.9765, alpha: 1.0)
-let darkGrey = UIColor(red: 0.698, green: 0.698, blue: 0.698, alpha: 1.0)
-let green = UIColor(red: 0.1098, green: 0.5294, blue: 0.2275, alpha: 1.0)
-let red = UIColor(red: 0.7294, green: 0.1529, blue: 0.1529, alpha: 1.0)
-let darkBlue = UIColor(red: 0.03, green: 0.22, blue: 0.39, alpha: 1.0)
+let startPoint = CGPointMake(50, 300)
+let endPoint = CGPointMake(300, 300)
+let controlPoint = CGPointMake(170, 200)
+let layer = CAShapeLayer()
+let path = UIBezierPath()
+path.moveToPoint(startPoint)
+path.addQuadCurveToPoint(endPoint, controlPoint: controlPoint)
+layer.path = path.CGPath
+layer.fillColor = UIColor.clearColor().CGColor
+layer.strokeColor = UIColor.redColor().CGColor
+let animation = CABasicAnimation(keyPath: "strokeEnd")
+animation.fromValue = 0
+animation.toValue = 1
+animation.duration = 2
+layer.addAnimation(animation, forKey: "")
+
+let container = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+container.layer.addSublayer(layer)
+container
+XCPlaygroundPage.currentPage.liveView = container
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+//XCPlaygroundPage.currentPage.finishExecution()
