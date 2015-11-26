0fa875e530bdb94b7f47784321e961568bbdb1a6
wangjinbian <wangjinbian@linggan.com>
2015-11-26 15:08:49 +0800

    no message

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -15,15 +15,21 @@ path.addQuadCurveToPoint(endPoint, controlPoint: controlPoint)
 layer.path = path.CGPath
 layer.fillColor = UIColor.clearColor().CGColor
 layer.strokeColor = UIColor.redColor().CGColor
-let animation = CABasicAnimation(keyPath: "strokeEnd")
-animation.fromValue = 0
-animation.toValue = 1
+let animation = CABasicAnimation(keyPath: "strokeStart")
+animation.fromValue = 0.5
+animation.toValue = 0
 animation.duration = 2
+
+let animation2 = CABasicAnimation(keyPath: "strokeEnd")
+animation2.fromValue = 0.5
+animation2.toValue = 1
+animation2.duration = 2
+//animation.autoreverses = true
+
 layer.addAnimation(animation, forKey: "")
+layer.addAnimation(animation2, forKey: "")
+
 
 let container = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
 container.layer.addSublayer(layer)
-container
 XCPlaygroundPage.currentPage.liveView = container
-XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
-//XCPlaygroundPage.currentPage.finishExecution()
