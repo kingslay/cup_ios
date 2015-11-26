e18bff5246cd8dd3d506a2e898ffa0c6ecb8ad7a
wangjinbian <kingslay@icloud.com>
2015-11-27 00:26:12 +0800

    no message

--- Source/Config/CupProvider.swift
+++ Source/Config/CupProvider.swift
@@ -40,8 +40,8 @@ public enum CupMoya {
     case Clock(String)
     case Temperature(String,Int)
 }
-let host = "http://121.199.75.79:8180/cup-0.1/"
-//let host = "http://localhost:8080/"
+//let host = "http://121.199.75.79:8180/cup-0.1/"
+let host = "http://localhost:8080/"
 
 extension CupMoya : MoyaTarget {
     public var baseURL: NSURL { return NSURL(string: host)! }
@@ -56,7 +56,7 @@ extension CupMoya : MoyaTarget {
         case .SaveMe:
             return "/user/saveme"
         case .Clock(_),.Temperature(_, _):
-            return "/behaviour/insert"
+            return "/behaviour"
         }
     }
     public var method: Moya.Method {
