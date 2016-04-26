00d2214ce4b7c4a61960e360dcf9ed81087dbdf0
wangjinbian <kingslay@icloud.com>
2016-04-26 11:36:02 +0800

    解决加载慢的问题

--- cup/AppDelegate.swift
+++ cup/AppDelegate.swift
@@ -18,8 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         Bugly.startWithAppId("900026229")
-
-        SMSSDK.registerApp("c1013d64d3ff", withSecret: "528dd34e0cb571afea389ae783053243")
 //        staticIdentifier = "80208298-6E62-076C-A59B-C0E0A1C9949C"
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         // 得到当前应用的版本号
@@ -59,6 +57,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UITabBar.appearance().barTintColor = Colors.black
         UITabBar.appearance().translucent = false
         configureAlamofireManager()
+        SMSSDK.registerApp("c1013d64d3ff", withSecret: "528dd34e0cb571afea389ae783053243")
         return true
     }
 
