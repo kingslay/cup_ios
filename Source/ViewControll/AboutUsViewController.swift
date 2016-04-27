07a4a6a5c337fdadcef60ec09616e36a7ac3396b
wangjinbian <kingslay@icloud.com>
2016-04-27 17:36:35 +0800

    no message

--- Source/ViewControll/AboutUsViewController.swift
+++ Source/ViewControll/AboutUsViewController.swift
@@ -26,7 +26,7 @@ class AboutUsViewController: UITableViewController {
             make.centerX.equalTo(tableHeaderView)
         }
         let lable = UILabel()
-        lable.text = "杭州未蓝智能科技有限公司"
+        lable.text = "红黄蓝工作室"
 //        \n杭州市萧山区兴五路237号\n0571－87703609"
         lable.numberOfLines = -1
         lable.textAlignment = .Center
@@ -57,10 +57,10 @@ extension AboutUsViewController {
         return cell
     }
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if indexPath.row == 0 {
-            UIApplication.sharedApplication().openURL(NSURL(string: "http://www.8amcup.com")!)
-        } else {
-            UIApplication.sharedApplication().openURL(NSURL(string: "https://shop152288103.taobao.com")!)
-        }
+//        if indexPath.row == 0 {
+//            UIApplication.sharedApplication().openURL(NSURL(string: "http://www.8amcup.com")!)
+//        } else {
+//            UIApplication.sharedApplication().openURL(NSURL(string: "https://shop152288103.taobao.com")!)
+//        }
     }
 }
